870dde121a0d9d5a6e46b326315da545659d5170
Maria Shady <maria.shady@yahoo.com>
2019-10-03 11:37:05 -0400

    file set up

---
+++ config/orm.js
@@ -0,0 +1,18 @@
+console.log("The ORM is ready!")
+
+const connection = require("./connection.js");
+
+const orm = {
+    select: (table, cb) => {
+        let query = "SELECT * FROM ??";
+
+        connection.query(query, [table], (err, results) => {
+            if(err) {
+                throw err;
+            }
+            console.log("ORM is displaying all burgers")
+            console.log(results);
+            cb(results)
+        });
+    }
+};
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Luger Burger", FALSE);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Le Pigeon Burger", FALSE);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Animal Style", TRUE);
