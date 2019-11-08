bc44a6925f3c591b50c33a8e05d0b4d454d5f93e
Maria Shady <maria.shady@yahoo.com>
2019-11-08 14:02:49 -0500

    heroku environment

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,23 @@
+require("dotenv");
 // Set up MySQL connection.
 const mysql = require("mysql");
+//  Set up Heroku connection.
+let connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "ilovelamp1",
-  database: "burger_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "ilovelamp1",
+    database: "burger_db"
+  });
+};
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
